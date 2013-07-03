84fa0d92e77852c478c524646e58f4b883cd47e5
Maciej Pasternacki <maciej@3ofcoins.net>
2013-07-03 15:35:03 +0200

    Tests

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
+name "sanitize"
+version "0.3.0"
+
 maintainer       "Maciej Pasternacki"
 maintainer_email "maciej@pasternacki.net"
 license          "MIT"
 description      "Sanitizes system by providing a sane default configuration"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.0"
 
 supports 'ubuntu', ">= 10.04"
 
